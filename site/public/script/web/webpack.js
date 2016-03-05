2d2e5d06d544dda204a135291056e4a78fd2ccf4
leapon <leapon@gmail.com>
2016-03-05 13:46:06 -0500

    cleanup webpack stuff

--- site/public/script/web/webpack.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-require('../../style/web/webpack.css');
-
-$().ready(function() {
-    console.log('in webpack test page');
-    var testOutput1 = 'Current time is: ' + require('./display-time.js');
-    $('.content1').html(testOutput1);
-});
--- site/public/script/web/webpack2.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-$().ready(function() {
-    console.log('in webpack test 2 page');
-    console.log('Current time is: ' + require('./display-time.js'));
-});
